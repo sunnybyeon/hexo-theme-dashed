e674dae9ea737cd4cfe4fc7e91f36a684b94cd89
sunnybyeon <sunnybyeon2631@gmail.com>
2024-05-26 15:38:10 +0900

    sort tags and categories too

--- scripts/lists.js
+++ scripts/lists.js
@@ -12,14 +12,20 @@ hexo.extend.generator.register("lists", (locals) => [
             layout: "posts",
         }
     ),
-    ...pagination(hexo.config.tag_dir, locals.tags, {
-        perPage: hexo.config.per_page,
-        format: `${paginationPath}/%d/`,
-        layout: "tags",
-    }),
+    ...pagination(
+        hexo.config.tag_dir,
+        locals.tags.sort(hexo.config.index_generator.order_by),
+        {
+            perPage: hexo.config.per_page,
+            format: `${paginationPath}/%d/`,
+            layout: "tags",
+        }
+    ),
     ...pagination(
         hexo.config.category_dir,
-        locals.categories.find({ parent: undefined }),
+        locals.categories
+            .find({ parent: undefined })
+            .sort(hexo.config.index_generator.order_by),
         {
             perPage: hexo.config.per_page,
             format: `${paginationPath}/%d/`,
